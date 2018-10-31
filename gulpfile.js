27da151a19d610c0790662bef22b4c609edb3345
Joan López <joan@netzstrategen.com>
2018-10-31 17:53:57 +0100

    fixup! Fixed Trusted Shop product reviews box was displayed when no reviews added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,8 @@
-var eol = require('gulp-eol');
-var eslint = require('gulp-eslint');
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
+/**
+ * Sample gulpfile.
+ * Gulp tasks declared in the gulp-task-collection module.
+ * Asset paths set in package.json.
+ */
 
-gulp.task('eslint', function () {
-  return gulp.src(['assets/scripts/**/*.js', '!node_modules/**', '!gulpfile.js'])
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
-
-gulp.task('scripts', ['eslint'], function () {
-  gulp.src('assets/scripts/**/*.js')
-  .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .on('error', function(e){
-      console.log(e);
-    })
-    .pipe(eol('\n'))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist/scripts'));
-});
-
-gulp.task('build', function(callback) {
-  runSequence('scripts', callback);
-});
-
-gulp.task('watch', function () {
-  gulp.watch('assets/scripts/**/*.js',['scripts']);
-});
-
-gulp.task('default', function () {
-  gulp.start('build');
-});
+const gulp = require('gulp');
+require('@netzstrategen/gulp-task-collection')(gulp);
--- src/Plugin.php
+++ src/Plugin.php
@@ -69,9 +69,34 @@ class Plugin {
    * @implements wp_enqueue_scripts.
    */
   public static function enqueueProductReviewsScripts() {
+    $git_version = static::getGitVersion();
+
     if (is_product()) {
-      wp_enqueue_script(static::PREFIX, Plugin::getBaseUrl() . '/dist/scripts/product-reviews.min.js', ['jquery'], FALSE, TRUE);
+      wp_enqueue_script(static::PREFIX, Plugin::getBaseUrl() . '/dist/scripts/main.js', ['jquery'], $git_version, TRUE);
     }
+  }
+
+  /**
+   * Generates a version out of the current commit hash.
+   *
+   * @return string
+   */
+  public static function getGitVersion() {
+    $git_version = NULL;
+    if (is_dir(ABSPATH . '.git')) {
+      $ref = trim(file_get_contents(ABSPATH . '.git/HEAD'));
+      if (strpos($ref, 'ref:') === 0) {
+        $ref = substr($ref, 5);
+        if (file_exists(ABSPATH . '.git/' . $ref)) {
+          $ref = trim(file_get_contents(ABSPATH . '.git/' . $ref));
+        }
+        else {
+          $ref = substr($ref, 11);
+        }
+      }
+      $git_version = substr($ref, 0, 8);
+    }
+    return $git_version;
   }
 
   /**
