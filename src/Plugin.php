5073d6ae744fd5d7b1b248768b2ed4e993db055b
Joan López <joan@netzstrategen.com>
2018-10-31 18:21:34 +0100

    Fixed Trusted Shop product reviews box was displayed when no reviews added. (#8)

--- src/Plugin.php
+++ src/Plugin.php
@@ -42,6 +42,9 @@ class Plugin {
    * @implements init
    */
   public static function init() {
+    // Enqueue Trusted Shops product reviews related scripts.
+    add_action('wp_enqueue_scripts', __CLASS__ . '::enqueueProductReviewsScripts');
+
     // Displays product rating stars after product title on product detail page.
     add_action('woocommerce_single_product_summary', __NAMESPACE__ . '\TrustedShops::woocommerce_single_product_summary', 6);
     // Displays product reviews on product detail page.
@@ -58,6 +61,42 @@ class Plugin {
     add_action(Plugin::PREFIX . '/badge/trusted-shops', __NAMESPACE__ . '\TrustedShops::renderBadge');
 
     add_filter('woocommerce_thankyou_order_received_text', __NAMESPACE__ . '\GoogleTrustedStores::woocommerce_thankyou_order_received_text', 100, 2);
+  }
+
+  /**
+   * Enqueues Trusted Shops product reviews related scripts.
+   *
+   * @implements wp_enqueue_scripts.
+   */
+  public static function enqueueProductReviewsScripts() {
+    $git_version = static::getGitVersion();
+
+    if (is_product()) {
+      wp_enqueue_script(static::PREFIX, Plugin::getBaseUrl() . '/dist/scripts/main.min.js', ['jquery'], $git_version, TRUE);
+    }
+  }
+
+  /**
+   * Generates a version out of the current commit hash.
+   *
+   * @return string
+   */
+  public static function getGitVersion() {
+    $git_version = NULL;
+    if (is_dir(ABSPATH . '.git')) {
+      $ref = trim(file_get_contents(ABSPATH . '.git/HEAD'));
+      if (strpos($ref, 'ref:') === 0) {
+        $ref = substr($ref, 5);
+        if (file_exists(ABSPATH . '.git/' . $ref)) {
+          $ref = trim(file_get_contents(ABSPATH . '.git/' . $ref));
+        }
+        else {
+          $ref = substr($ref, 11);
+        }
+      }
+      $git_version = substr($ref, 0, 8);
+    }
+    return $git_version;
   }
 
   /**
