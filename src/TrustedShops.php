de42c7b59346f13ab0cd1aeb43c3c8e41ddc4418
Bogdan Arizancu <bogdanarizancu@gmail.com>
2020-04-09 14:55:18 +0200

    Improved performance by avoiding WC_Product::get_available_variations().

--- src/TrustedShops.php
+++ src/TrustedShops.php
@@ -183,7 +183,7 @@ EOD;
    * @implements wp_footer
    */
   public static function wp_footer() {
-    global $product;
+    global $product, $wpdb;
 
     if (!$shop_id = Settings::getOption('trusted_shops/id')) {
       return;
@@ -225,10 +225,15 @@ EOD;
 
     $products_sku = [];
     if ($product->get_type() === 'variable') {
-      $variations = $product->get_available_variations();
-      foreach ($variations as $variation) {
-        $products_sku[] = $variation['sku'] ?? '';
-      }
+      // Avoid WC_Product_Variable::get_available_variations() as it
+      // additionally produces a lot of output for templates, unnecessary here.
+      $variation_ids = $product->get_visible_children();
+      $placeholders = implode(',', array_fill(0, count($variation_ids), '%d'));
+      $products_sku = $wpdb->get_col($wpdb->prepare("
+        SELECT pm.meta_value AS sku
+        FROM wp_postmeta pm
+        WHERE pm.post_id IN ($placeholders) AND pm.meta_key = '_sku'
+      ", $variation_ids));
     }
     else {
       $products_sku[] = $product->get_sku() ?? '';
