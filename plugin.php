6d956968beab20000857039c567fdae2c4fd6d79
Luca Pipolo <luca@netzstrategen.com>
2020-04-15 17:45:36 +0200

    fixup! Fixed schema.org ID was missing on Trusted Shops rich snippet.

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: WooCommerce Reputations
   Plugin URI: https://github.com/netzstrategen/woocommerce-reputations
-  Version: 1.5.1
+  Version: 1.5.2
   Text Domain: woocommerce-reputations
   Description: Integrate Trusted Shops and Google Trusted Stores into WooCommerce.
   Author: netzstrategen
