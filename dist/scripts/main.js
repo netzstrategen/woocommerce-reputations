7a16e70374c35838c1b8192594621dc1ea28927d
Callum <callum@netzstrategen.com>
2020-11-03 11:18:00 +0100

    Fixed broken frontend functionality due to missing JS file.

---
+++ dist/scripts/main.js
@@ -0,0 +1,35 @@
+(function pageLoad($) {
+  var $reviewsBox = $('#woocommerce-reputations-trusted-shops-product-reviews');
+  /**
+   * Hides Trusted Shops product reviews box if it does not contain reviews.
+   *
+   * @param {Array} mutations Changes to Trusted Shops product reviews box.
+   */
+
+  function MutationObserverCallBack(mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.addedNodes !== null && $reviewsBox.find('.ts-no-reviews').length) {
+        $reviewsBox.css('display', 'none');
+      }
+    });
+  }
+  /**
+   * Starts a mutation observer on the product container element
+   * to hide the Trusted Shops products reviews box if it does not
+   * contain reviews.
+   *
+   * @return {Object} Mutation observer
+   */
+
+
+  function observeProductReviewsBox() {
+    var observer = new MutationObserver(MutationObserverCallBack);
+    return observer.observe($reviewsBox[0], {
+      childList: true
+    });
+  }
+
+  if ($reviewsBox.length) {
+    observeProductReviewsBox();
+  }
+})(jQuery);
